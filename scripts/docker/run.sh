3ab05194edbe3aa5062ede3923928911671547b5
anmol26s <github@datamol.org>
2020-11-30 02:57:57 +0530

    update 2.0.0

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-[ "$architecture" == "amd64" ] && image=portainer/portainer:1.20.1
-[ "$architecture" == "i386" ]  && image=portainer/portainer:linux-386-1.20.1
-[ "$architecture" == "armhf" ] && image=portainer/portainer:linux-arm-1.20.1
+[ "$architecture" == "amd64" ] && image=portainer/portainer-ce:2.0.0
+[ "$architecture" == "i386" ]  && image=portainer/portainer-ce:linux-386-2.0.0
+[ "$architecture" == "armhf" ] && image=portainer/portainer-ce:linux-arm-2.0.0
 [ -z $image ] && ynh_die "Sorry, your $architecture architecture is not supported ..."
 
 options="-p $port:9000 -v $data_path/data:/data -v /var/run/docker.sock:/var/run/docker.sock"
 containeroptions="--no-auth"
 
-iptables -t filter -N DOCKER 
+iptables -t filter -N DOCKER
 
 docker run -d --name=$app --restart always $options $image $containeroptions 1>&2
 CR=$?
